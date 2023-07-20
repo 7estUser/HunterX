d2148183e10767f7807242736dff67a4084e0101
GitHubTest <test.github.com>
2023-07-20 15:44:54 +0800

    结果导出文件名称bug修改

--- util/tools.go
+++ util/tools.go
@@ -4,24 +4,8 @@ import (
 	"encoding/base64"
 	"math/rand"
 	"strings"
+	"time"
 )
-
-var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-//生成随机字符串
-func RandomString(n int, allowedChars ...[]rune) string {
-	var letters []rune
-	if len(allowedChars) == 0 {
-		letters = defaultLetters
-	} else {
-		letters = allowedChars[0]
-	}
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
 
 //base64 url编码
 func base64UrlEncode(input string) string {
@@ -30,10 +14,10 @@ func base64UrlEncode(input string) string {
 
 func OutFileName(query string) string {
 	startIndex := strings.Index(query, "=\"")
-	endIndex := strings.Index(query[startIndex+1:], "\"")
+	endIndex := strings.Index(query[startIndex+2:], "\"")
 	randomString := generateRandomString(4)
 	if startIndex != -1 && endIndex != -1 && endIndex > startIndex {
-		return query[startIndex+1:endIndex] + "-" + randomString
+		return query[startIndex+2:startIndex+2+endIndex] + "-" + randomString
 	} else {
 		return randomString + "-" + randomString
 	}
@@ -43,6 +27,7 @@ func OutFileName(query string) string {
 func generateRandomString(length int) string {
 	chars := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 	result := make([]byte, length)
+	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < length; i++ {
 		result[i] = chars[rand.Intn(len(chars))]
 	}
